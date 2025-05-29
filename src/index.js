5871f64557af12c42276eb418b18ad51a7de65fc
madhukodali23 <madhukanth838@gmail.com>
2025-05-29 03:42:32 +0000

    added base name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/EBank">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
